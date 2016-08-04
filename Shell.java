d347a13e6fbca2f9879277d1e00a6874bed806e3
ynw <ynw_soft@yahoo.co.jp>
2016-08-05 07:00:00 +0900

    sub arg error disp sub options

--- Shell.java
+++ Shell.java
@@ -2,21 +2,31 @@ import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.spi.SubCommandHandler;
+import org.kohsuke.args4j.spi.SubCommand;
+import org.kohsuke.args4j.spi.SubCommands;
 import org.kohsuke.args4j.spi.StringArrayOptionHandler;
 
 public class Shell {
+    // コマンド本体のオプション
     @Option(name="-v", aliases="--version", usage="print version")
         private boolean versionFlag;
-
     @Option(name="-h", aliases="--help", usage="print usage message and exit")
         private boolean usageFlag;
 
-    @Argument(index=0, metaVar="script")
-        private String script;
+    @Argument(
+        usage="subcommand",
+        handler = SubCommandHandler.class
+    )
 
-    @Argument(index=1, metaVar="arguments...", handler=StringArrayOptionHandler.class)
-        private String[] arguments;
+    // サブコマンド定義
+    @SubCommands({
+        @SubCommand(name = "sub1", impl = Sub1Command.class),
+        @SubCommand(name = "sub2", impl = Sub2Command.class)
+    })
+    private Command command;
 
+    // コマンド本体
     public static void main(String[] args) {
         Shell shell = new Shell();
 
@@ -24,25 +34,93 @@ public class Shell {
         try {
             parser.parseArgument(args);
         } catch (CmdLineException e) {
-            e.printStackTrace();
+            System.out.println(e);
+            System.out.println();
+            e.getParser().printUsage(System.out);
             return;
         }
 
         if (shell.usageFlag) {
-            System.out.println("Usage:");
-            System.out.println(" Shell [options]");
-            System.out.println(" Shell [options] script [arguments]");
-            System.out.println();
+            printUsage();
             System.out.println("Options:");
             parser.printUsage(System.out);
             return;
         }
 
         if (shell.versionFlag) {
-            System.out.println("Sample Program ver 1.0.0");
+            printVersion();
             return;
         }
 
-        System.out.println("finish");
+        // サブコマンドが指定されていたら実行する
+        if(shell.command != null){
+            shell.command.execute();
+            return;
+        }
+        printUsage();
+    }
+
+    private static void printUsage(){
+        System.out.println("Usage:");
+        System.out.println(" Shell [options]");
+        System.out.println(" Shell subcommand [subcommand options]");
+        System.out.println();
+        System.out.println("Subcommand:");
+        System.out.println(" sub1: hogehoge");
+        System.out.println(" sub2: hugahuga");
+        System.out.println();
+    }
+
+    private static void printVersion(){
+        System.out.println("sample version 0.0.1");
+    }
+
+    /**
+     * コマンド
+     */
+    public static interface Command {
+        public void execute();
+    }
+
+    /**
+     * Sub1
+     */
+    public static class Sub1Command implements Command {
+        @Option(name="-i", aliases="--input", required = true, usage="input file path")
+            private String inputPath;
+        @Option(name="-o", aliases="--output", usage="output file path")
+            private String outputPath;
+        @Option(name="-h", aliases="--help", usage="print usage message and exit")
+            private boolean usageFlag;
+
+        @Override
+        public void execute() {
+            if (usageFlag) {
+                printUsage();
+                return;
+            }
+
+            System.out.println("Sub1");
+            System.out.println("input  : " + inputPath);
+            System.out.println("output : " + outputPath);
+        }
+
+        public void printUsage(){
+            Sub1Command sub1 = new Sub1Command();
+            CmdLineParser parser = new CmdLineParser(sub1);
+            System.out.println("Sub1 help");
+            System.out.println("Options:");
+            parser.printUsage(System.out);
+        }
+    }
+
+    /**
+     * Sub2
+     */
+    public static class Sub2Command implements Command {
+        @Override
+        public void execute() {
+            System.out.println("Sub2");
+        }
     }
 }
--- test.sh
+++ test.sh
@@ -1,2 +1,20 @@
+sub(){
+    echo "################################################################################"
+    echo "## " "$1"
+    echo "################################################################################"
+    java -classpath ./:args4j-2.33.jar Shell $*
+}
+
 javac -classpath args4j-2.33.jar Shell.java
-java -classpath ./:args4j-2.33.jar Shell
+
+sub
+sub "-v"
+sub "-h"
+sub "sub1"
+sub "sub1 -h"
+sub "sub1 -i aaa -o bbb"
+sub "sub2"
+sub "sub2 -h"
+sub "hoge"
+sub "sub1 -s"
+
